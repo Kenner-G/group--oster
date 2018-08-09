5ec0e2e82cd1b84a6b9fefd83fa14899b4c15418
Kenner <kenner.galdamez@gmail.com>
2018-08-09 01:10:19 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<form name=”web_form” id=”web_form” method=”post” action=”process-form-data.php”>
+<h1>Hazel Cafe Setup - Rank Management / Check Rank</h1>
+        <p><label>Group ID:</label><input type=”text” name=”name” id=”name” /></p>
+        <p><label>Group holder ranking cookie:</label><input type=”text” name=”email” id=”email” /></p>
+        <p><input type=”submit” name=”s1″ id=”s1″ value=”Submit” /></p>
+</form>
